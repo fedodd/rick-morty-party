da87b00c3ff8f975cd987212ed003861e7c67feb
fedodd <fedodd.s@gmail.com>
2020-02-05 21:08:42 +0300

    throttle form changes

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,30 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classes from "./form.pcss";
-//import useRickMortySearch from "./../../hooks/useRickMortySearch";
-
+import _ from 'lodash';
 
 function Form(props) {
 
+  //set value only after it's ready to make api request
   const [value, setValue] = useState('');
-  useEffect(() => {
-    console.log('useEffect', value, props);
-    props.handleChangeInput(value);
-  });
 
-  function useOnChange(value) {
-    console.log('useChange', value);
+  // throttle - send value to app component after 300 ms. listen prev value with useRef
+  const throttled = useRef(_.throttle((newValue) => {
+    props.handleChangeInput(newValue);
+  }, 300))
+
+
+  useEffect(() => {
+    throttled.current(value);
+  }, [value]);
+
+  //send changes only if length >2
+  const handleChange = (value) => {
     if (value.length > 2) {
       setValue(value);
-    }
+    };
   }
 
   return (
     <form
+      onSubmit={event => event.preventDefault()}
       className={classes.form}>
       <input
         type="text"
         name="name"
-        onChange={(e) => useOnChange(e.target.value)}
+        onChange={(e) => handleChange(e.target.value)}
         required >
       </input>
     </form>
--- src/components/results/results.js
+++ src/components/results/results.js
@@ -2,6 +2,7 @@ import React from 'react';
 import useRickMortySearch from "../../hooks/useRickMortySearch.js";
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import _ from 'lodash';
 
 const RICK_MORTY_SEARCH = gql`
   query Name($targetName: String) {
@@ -20,27 +21,36 @@ const RICK_MORTY_SEARCH = gql`
 
 function Results(props) {
 
-  console.log('results name', props.requestName);
-  const targetName = props.requestName;
-  const { loading, error, data } = useQuery(RICK_MORTY_SEARCH, {
-    variables: { targetName },
-  });
+  let result = <p>Search results will be shown here.</p>;
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
+  const APIrequest = (targetName) => {
 
-  if (!data.characters.info.count) return <p>No results</p>;
+    if (targetName.length <= 2) {
+      return result;
+    }
 
-  let resultList = data.characters.results.map(elem =>
-    (<div key={elem.id}>
-      <h3>{elem.name}</h3>
-      <img src={elem.image}></img>
-    </div>)
-  );
+    const { loading, error, data } = useQuery(RICK_MORTY_SEARCH, {
+      variables: { targetName },
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+
+    if (!data.characters.info.count) return <p>No results</p>;
+
+    return data.characters.results.map(elem =>
+      (<div key={elem.id}>
+        <h3>{elem.name}</h3>
+        <img src={elem.image}></img>
+      </div>)
+    );
+  }
+
+  result = APIrequest(props.requestName);
 
   return (
     <div>
-      {resultList}
+      {result}
     </div>
   )
 
